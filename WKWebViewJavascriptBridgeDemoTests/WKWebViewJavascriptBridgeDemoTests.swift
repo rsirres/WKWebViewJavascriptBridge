948aa44ee8b393f5f8f9ea189680e1cbc82b6bd7
Lision <lisionmail@gmail.com>
2020-05-16 18:53:59 +0800

    本地调试完毕

--- WKWebViewJavascriptBridgeDemoTests/WKWebViewJavascriptBridgeDemoTests.swift
+++ WKWebViewJavascriptBridgeDemoTests/WKWebViewJavascriptBridgeDemoTests.swift
@@ -1,36 +1,34 @@
 //
-//  WKWebViewJavascriptBridgeTests.swift
-//  WKWebViewJavascriptBridgeTests
+//  WKWebViewJavascriptBridgeDemoTests.swift
+//  WKWebViewJavascriptBridgeDemoTests
 //
-//  Created by Lision on 2018/1/20.
-//  Copyright © 2018年 Lision. All rights reserved.
+//  Created by 李鑫 on 2020/5/16.
+//  Copyright © 2020 lision. All rights reserved.
 //
 
 import XCTest
-@testable import WKWebViewJavascriptBridge
+@testable import WKWebViewJavascriptBridgeDemo
 
-class WKWebViewJavascriptBridgeTests: XCTestCase {
-    
-    override func setUp() {
-        super.setUp()
+class WKWebViewJavascriptBridgeDemoTests: XCTestCase {
+
+    override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-    
-    override func tearDown() {
+
+    override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
     }
-    
-    func testExample() {
+
+    func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
-    
-    func testPerformanceExample() {
+
+    func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-    
+
 }
